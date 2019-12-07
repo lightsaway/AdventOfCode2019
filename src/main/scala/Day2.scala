f8a7999b817b7ed758da244154b1999c5a860f8b
Anton Serhiienko <anton.serhiienko@klarna.com>
2019-12-07 19:20:51 +0200

    day2 part2

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -9,7 +9,7 @@ object Day2 extends IOApp {
     case List(a, b, o) => (a, b, o)
   }
 
-  def runProgram(idx: Int, prog: Program): Program =
+  def runProgram(idx: Int, prog: Program): Int =
     prog(idx) match {
       case 1 =>
         val (a, b, o) = read(idx, prog)
@@ -17,29 +17,50 @@ object Day2 extends IOApp {
       case 2 =>
         val (a, b, o) = read(idx, prog)
         runProgram(idx + 4, prog.updated(o, prog(a) * prog(b)))
-      case 99 => prog
+      case 99 => prog.head
       case _  => throw new IllegalArgumentException("unknown operation")
     }
 
   def restore(p: Program): Program = p.updated(1, 12).updated(2, 2)
 
+  def gravityAssist(p: Program): Int =
+    fs2.Stream
+      .emits(0.until(99))
+      .flatMap { noun =>
+        fs2.Stream.emits(0.until(99)).map { verb =>
+          {
+            val memory = p.updated(1, noun).updated(2, verb)
+            val out    = runProgram(0, memory)
+            (noun, verb, out)
+          }
+        }
+      }
+      .dropWhile { case (_, _, o) => o != 19690720 }
+      .take(1)
+      .map { case (n, v, o) => 100 * n + v }
+      .compile
+      .toList
+      .head
+
   def toProgram(s: String): Program = s.split(",").toList.map(_.toInt)
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val records = Utils
+    val data = Utils
       .readStrings("./day2.txt")
       .compile
       .toList
+      .map(_.head)
 
-    records
-      .map { strings =>
-        strings
-          .map(toProgram)
-          .map(restore)
-          .map(p => runProgram(0, p))
-      }
-      .map(progs => progs.map(_.head).map(println))
-      .as(ExitCode.Success)
+    val programm = data.map(toProgram)
+
+    programm
+      .map(restore)
+      .map(p => runProgram(0, p))
+      .map(println) *>
+      programm
+        .map(gravityAssist)
+        .map(println) *>
+      IO.unit.as(ExitCode.Success)
   }
 
 }
